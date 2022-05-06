cecae42f10c7d8106fa5cd1093fdacfc4817660b
Andrii Maglovanyi <andrii.maglovanyi@vonage.com>
2022-05-06 09:24:58 +0100

    chore: add state param

--- src/AuthService.ts
+++ src/AuthService.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import { createPKCECodes, PKCECodePair } from './pkce'
+import { createPKCECodes, PKCECodePair, base64URLEncode } from './pkce'
 import { toUrlEncoded } from './util'
+import randomBytes from 'randombytes'
 
 import jwtDecode from 'jwt-decode'
 
@@ -147,18 +148,20 @@ export class AuthService<TIDToken = JWTIDToken> {
     return window.localStorage.getItem('auth') !== null
   }
 
-  async logout(shouldEndSession: boolean = false): Promise<boolean> {
+  async logout(shouldEndSession = false): Promise<boolean> {
     this.removeItem('pkce')
     this.removeItem('auth')
     if (shouldEndSession) {
-      const { clientId, provider, logoutEndpoint, redirectUri } = this.props;
+      const { clientId, provider, logoutEndpoint, redirectUri } = this.props
       const query = {
         client_id: clientId,
         post_logout_redirect_uri: redirectUri
       }
-      const url = `${logoutEndpoint || `${provider}/logout`}?${toUrlEncoded(query)}`
+      const url = `${logoutEndpoint || `${provider}/logout`}?${toUrlEncoded(
+        query
+      )}`
       window.location.replace(url)
-      return true;
+      return true
     } else {
       window.location.reload()
       return true
@@ -171,9 +174,18 @@ export class AuthService<TIDToken = JWTIDToken> {
 
   // this will do a full page reload and to to the OAuth2 provider's login page and then redirect back to redirectUri
   authorize(): boolean {
-    const { clientId, provider, authorizeEndpoint, redirectUri, scopes, audience } = this.props
+    const {
+      clientId,
+      provider,
+      authorizeEndpoint,
+      redirectUri,
+      scopes,
+      audience
+    } = this.props
 
     const pkce = createPKCECodes()
+    const state = base64URLEncode(randomBytes(16))
+    window.localStorage.setItem('pkce_state', state)
     window.localStorage.setItem('pkce', JSON.stringify(pkce))
     window.localStorage.setItem('preAuthUri', location.href)
     window.localStorage.removeItem('auth')
@@ -186,10 +198,13 @@ export class AuthService<TIDToken = JWTIDToken> {
       redirectUri,
       ...(audience && { audience }),
       codeChallenge,
-      codeChallengeMethod: 'S256'
+      codeChallengeMethod: 'S256',
+      state
     }
     // Responds with a 302 redirect
-    const url = `${authorizeEndpoint || `${provider}/authorize`}?${toUrlEncoded(query)}`
+    const url = `${authorizeEndpoint || `${provider}/authorize`}?${toUrlEncoded(
+      query
+    )}`
     window.location.replace(url)
     return true
   }
@@ -237,10 +252,14 @@ export class AuthService<TIDToken = JWTIDToken> {
       body: toUrlEncoded(payload)
     })
     this.removeItem('pkce')
-    let json = await response.json()
+    const json = await response.json()
     if (isRefresh && !json.refresh_token) {
       json.refresh_token = payload.refresh_token
     }
+    if (json.state && json.state !== this.getItem('pkce_state')) {
+      console.warn(`State ${json.state} doesn't match`)
+    }
+    this.removeItem('pkce_state')
     this.setAuthTokens(json as AuthTokens)
     if (autoRefresh) {
       this.startTimer()
